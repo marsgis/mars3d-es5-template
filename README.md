c6cd06367ea9dd1b6080ebb368f75b1dd74a90f9
muyao <muyao1987@qq.com>
2020-02-10 17:12:31 +0800

    初始化开源版本

---
+++ README.md
@@ -0,0 +1,21 @@
+# Mars3D项目模版 - ES5传统版
+ 传统模式Web前端技术栈下的最简[Mars3D](http://cesium.marsgis.cn)应用的三维地球项目模版
+  
+
+
+## 使用说明
+ 在任意开发编辑器（如vscode等）或http服务器(如node、nginx、tomcat、IIS等)下直接运行浏览index.html即可
+
+### 压缩及混淆
+ build整站压缩及混淆：[https://github.com/muyao1987/web-dist](https://github.com/muyao1987/web-dist)
+
+
+## 运行效果
+ demo系统： [http://cesium.marsgis.cn/demo.html](http://cesium.marsgis.cn/demo.html)
+
+ ![image](http://cesium.marsgis.cn/docs/img/project/1.jpg)
+ 
+ 
+## 版权说明
+  本项目主要是为了展示[Mars3D](http://cesium.marsgis.cn)的项目应用，仅限大家学习之用，如需用于商业项目，请联系购买[http://cesium.marsgis.cn](火星科技)SDK授权。
+ 并且Mars3D-SDK类库并未开源（即`libs/cesiumjs/mars3d/`）,内部有作者公司logo及时效限制。
---
+++ js/index.js
@@ -0,0 +1,164 @@
+//系统 主入口
+
+var viewer; //地球对象
+//地图
+$(document).ready(function () {
+    if (!mars3d.util.webglreport()) {
+        toastr.error('系统检测到您当前使用的浏览器WebGL功能无效');
+        layer.open({
+            type: 1,
+            title: "当前浏览器WebGL功能无效",
+            skin: "layer-mars-dialog animation-scale-up",
+            resize: false,
+            area: ['600px', '200px'], //宽高
+            content: '<div style="margin: 20px;"><h3>系统检测到您使用的浏览器WebGL功能无效！</h3>  <p>1、请您检查浏览器版本，安装使用最新版chrome、火狐或IE11以上浏览器！</p> <p>2、WebGL支持取决于GPU支持，请保证客户端电脑已安装最新显卡驱动程序！</p><p>3、如果上两步骤没有解决问题，说明您的电脑需要更换了！</p></div>'
+        });
+    }
+    initUI();
+    initMap();
+});
+
+function removeMask() {
+    $("#mask").remove();
+}
+
+
+//初始化地图
+function initMap() {
+
+    mars3d.createMap({
+        id: 'cesiumContainer',
+        url: "config/config.json",
+        //infoBox: false,     //是否显示点击要素之后显示的信息  【也可以在config.json中配置】  
+        //sceneMode: Cesium.SceneMode.SCENE2D, 
+        success: function (_viewer, jsondata) {//地图成功加载完成后执行 
+            //欢迎UI关闭处理
+            setTimeout(removeMask, 3000);
+
+            //记录viewer
+            viewer = _viewer;
+
+            viewer.mars.openFlyAnimation();//开场动画
+
+            initWork(_viewer);
+        }
+    });
+}
+
+
+//UI界面相关
+function initUI() {
+    haoutil.oneMsg('首次访问系统无缓存会略慢，请耐心等待！', 'load3d_tip');
+
+
+}
+
+
+//当前页面业务相关
+function initWork(viewer) {
+    haoutil.oneMsg('如果未出现地球，是因为地形加载失败，请刷新重新加载！', 'terrain_tip');
+
+    // 禁用默认的实体双击动作。
+    viewer.screenSpaceEventHandler.removeInputAction(Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK);
+    viewer.screenSpaceEventHandler.removeInputAction(Cesium.ScreenSpaceEventType.LEFT_CLICK);
+
+    //鼠标滚轮放大的步长参数
+    viewer.scene.screenSpaceCameraController._zoomFactor = 1.5;
+
+    //webgl渲染失败后，刷新页面
+    //viewer.scene.renderError.addEventListener(function (scene, error) {
+    //    window.location.reload();
+    //});
+
+
+    //移动设备上禁掉以下几个选项，可以相对更加流畅
+    if (!haoutil.system.isPCBroswer()) {
+        viewer.targetFrameRate = 20;        //限制帧率
+        viewer.requestRenderMode = true;    //取消实时渲染
+        viewer.scene.fog.enable = false;
+        viewer.scene.skyAtmosphere.show = false;
+        viewer.scene.fxaa = false;
+    }
+
+    //IE浏览器优化
+    if (window.navigator.userAgent.toLowerCase().indexOf("msie") >= 0) {
+        viewer.targetFrameRate = 20;        //限制帧率
+        viewer.requestRenderMode = true;    //取消实时渲染
+    }
+
+
+    //二三维切换不用动画
+    if (viewer.sceneModePicker)
+        viewer.sceneModePicker.viewModel.duration = 0.0;
+
+    //设置操作习惯,更换中键和右键 
+    //viewer.scene.screenSpaceCameraController.tiltEventTypes = [
+    //    Cesium.CameraEventType.RIGHT_DRAG, Cesium.CameraEventType.PINCH,
+    //    { eventType: Cesium.CameraEventType.LEFT_DRAG, modifier: Cesium.KeyboardEventModifier.CTRL },
+    //    { eventType: Cesium.CameraEventType.RIGHT_DRAG, modifier: Cesium.KeyboardEventModifier.CTRL }
+    //];
+    //viewer.scene.screenSpaceCameraController.zoomEventTypes = [Cesium.CameraEventType.MIDDLE_DRAG, Cesium.CameraEventType.WHEEL, Cesium.CameraEventType.PINCH];
+
+
+    $.ajax({
+        type: "get",
+        dataType: "json",
+        url: 'data/point.json',
+        timeout: 0,
+        success: function (data) {
+            showPoint(data.Data)
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            haoutil.loading.hide();
+            haoutil.alert(configfile + "文件加载失败！");
+        }
+    });
+
+
+}
+
+
+function showPoint(arrdata) {
+    var dataSource = new Cesium.CustomDataSource();
+    viewer.dataSources.add(dataSource);
+
+    for (var item of arrdata) {
+        var position = Cesium.Cartesian3.fromDegrees(item.JD, item.WD, 2);
+
+        //添加实体
+        var entity = dataSource.entities.add({
+            name: item.JC,
+            position: position,
+            point: {
+                //像素点
+                color: new Cesium.Color.fromCssColorString("#3388ff"),
+                pixelSize: 10,
+                outlineColor: new Cesium.Color.fromCssColorString("#ffffff"),
+                outlineWidth: 2,
+                scaleByDistance: new Cesium.NearFarScalar(1000, 1, 1000000, 0.1)
+            },
+            label: {
+                text: item.JC,
+                font: "16px 楷体",
+                style: Cesium.LabelStyle.FILL_AND_OUTLINE,
+                fillColor: Cesium.Color.AZURE,
+                outlineColor: Cesium.Color.BLACK,
+                outlineWidth: 2,
+                horizontalOrigin: Cesium.HorizontalOrigin.CENTER,
+                verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
+                pixelOffset: new Cesium.Cartesian2(0, -10), //偏移量
+                distanceDisplayCondition: new Cesium.DistanceDisplayCondition(
+                    0.0,
+                    200000
+                )
+            },
+            data: item,
+            popup: mars3d.util.getPopup("all", item, "企业点"),
+            click: function (entity) {
+                //单击回调
+                haoutil.msg("您单击了：" + entity.data.JC);
+            }
+        });
+    }
+
+}
