6d05bee24eecb265c92c4dc4922f00e5b245f1e8
wanghao <muyao1987@qq.com>
2021-02-05 12:14:12 +0800

    升级到V3.0

--- README.md
+++ README.md
@@ -1,28 +1,34 @@
-# Mars3D最简项目模版 - ES5传统版
- 传统模式Web前端技术栈下的最简[Mars3D](http://cesium.marsgis.cn)应用的三维地球项目模版。
+# Mars3D最简项目模版 - ES5传统版  
+    Mars3D三维地球平台软件，在`传统JS技术栈`的最简的应用项目模版。
   
- 其他技术栈，请参考 [Mars3D开源导航](https://github.com/marsgis/mars3d)
+ > 其他技术栈，请参考： [https://github.com/marsgis/mars3d](https://github.com/marsgis/mars3d)
  
  
 
-## 使用说明
+## 运行站点
  在任意开发编辑器（如vscode等）或http服务器(如node、nginx、tomcat、IIS等)下直接运行浏览index.html即可
 
-### 压缩及混淆
- build整站压缩及混淆：[https://github.com/muyao1987/web-dist](https://github.com/muyao1987/web-dist)
+### 运行效果 
+ [在线Demo](http://mars3d.cn/project/es5-template/)  
 
-
-## 运行效果 
- [在线Demo](http://cesium.marsgis.cn/project/simple-es5/index.html)  
-
- ![image](http://cesium.marsgis.cn/project/img/simple-es5.jpg)
+ ![image](http://mars3d.cn/project/es5-template/screenshot.jpg)
  
- [更多项目体验](http://cesium.marsgis.cn/project.html)
 
- 
- 
+
+## 压缩及混淆
+ 如果需要编译、对整站压缩及混淆，请参考：[https://github.com/muyao1987/web-dist](https://github.com/muyao1987/web-dist)
+
+
+
+## Mars3D 是什么 
+> `Mars3D三维地球平台软件` 是[火星科技](http://marsgis.cn/)团队研发的二三维一体的WebGIS地图开发平台，是火星科技团队成员多年GIS开发和Cesium使用的技术沉淀。基于[Cesium](https://cesium.com/cesiumjs/)开源库和现代Web技术栈全新构建，该平台框架优化了Cesium的使用方式和增添了更多高级功能。集成了领先的开源地图库、可视化库，提供了全新的三维大数据可视化、实时流数据可视化功能，通过本产品可快速实现浏览器和移动端上美观、流畅的三维地图呈现与空间分析。
+
+### 相关网站 
+- Mars3D官网：[http://mars3d.cn](http://mars3d.cn)  
+
+- GitHub导航列表：[https://github.com/marsgis/mars3d](https://github.com/marsgis/mars3d)
 
 
 ## 版权说明
-1. 当前[Mars3D](http://cesium.marsgis.cn)免费版本可以免费无限制使用.
-2. 如有更高需求或商业应用，请联系购买[火星科技](http://cesium.marsgis.cn)进行SDK授权(去除logo、添加授权信息等)。 
+1. 任何`个人或组织`可以在遵守Mars3D相关要求下`免费无限制`使用。
+2. 如有`个性化需求`或`商业应用`，请联系[火星科技](http://mars3d.cn)购买。
--- js/index.js
+++ js/index.js
@@ -1,157 +1,333 @@
-//系统 主入口
+'use script' //开发环境建议开启严格模式
 
-var viewer; //地球对象
-//地图
-$(document).ready(function () {
-    if (!mars3d.util.webglreport()) {
-        toastr.error('系统检测到您当前使用的浏览器WebGL功能无效');
-        layer.open({
-            type: 1,
-            title: "当前浏览器WebGL功能无效",
-            skin: "layer-mars-dialog animation-scale-up",
-            resize: false,
-            area: ['600px', '200px'], //宽高
-            content: '<div style="margin: 20px;"><h3>系统检测到您使用的浏览器WebGL功能无效！</h3>  <p>1、请您检查浏览器版本，安装使用最新版chrome、火狐或IE11以上浏览器！</p> <p>2、WebGL支持取决于GPU支持，请保证客户端电脑已安装最新显卡驱动程序！</p><p>3、如果上两步骤没有解决问题，说明您的电脑需要更换了！</p></div>'
-        });
-    }
-    initUI();
-    initMap();
-});
-
-function removeMask() {
-    $("#mask").remove();
+//判断webgl支持
+if (!mars3d.util.webglreport()) {
+    mars3d.util.webglerror()
 }
 
+//读取 config.json 配置文件
+var configUrl = 'config/config.json'
+mars3d.Resource.fetchJson({ url: configUrl }).then(function (json) {
+    initMap(json.map3d)//构建地图
+})
 
-//初始化地图
-function initMap() {
 
-    mars3d.createMap({
-        id: 'mars3dContainer',
-        url: "config/config.json",
-        //infoBox: false,     //是否显示点击要素之后显示的信息  【也可以在config.json中配置】  
-        //sceneMode: Cesium.SceneMode.SCENE2D, 
-        success: function (_viewer, jsondata) {//地图成功加载完成后执行 
-            //欢迎UI关闭处理
-            setTimeout(removeMask, 3000);
 
-            //记录viewer
-            viewer = _viewer;
+var map
 
-            viewer.mars.openFlyAnimation();//开场动画
+function initMap (mapOptions) {
+    //创建三维地球场景
+    map = new mars3d.Map('mars3dContainer', mapOptions)
 
-            initWork(_viewer);
-        }
+
+    //以下为演示代码
+
+    //创建entity图层
+    let graphicLayer = new mars3d.layer.GraphicLayer();
+    map.addLayer(graphicLayer);
+
+    //2.在layer上绑定监听事件
+    graphicLayer.on(mars3d.EventType.click, function (event) {
+        console.log("监听layer，单击了矢量对象", event);
     });
-}
+    graphicLayer.on(mars3d.EventType.mouseOver, function (event) {
+        console.log("监听layer，鼠标移入了矢量对象", event);
+    });
+    graphicLayer.on(mars3d.EventType.mouseOut, function (event) {
+        console.log("监听layer，鼠标移出了矢量对象", event);
+    });
 
+    //可在图层上绑定popup,对所有加到这个图层的矢量数据都生效
+    graphicLayer.bindPopup("我是layer上绑定的Popup", {
+        anchor: [0, -10],
+    });
 
-//UI界面相关
-function initUI() {
-    haoutil.oneMsg('首次访问系统无缓存会略慢，请耐心等待！', 'load3d_tip');
-
-
-}
-
-
-//当前页面业务相关
-function initWork(viewer) {
-    haoutil.oneMsg('如果未出现地球，是因为地形加载失败，请刷新重新加载！', 'terrain_tip');
-
-
-    //针对不同终端的优化配置
-    if (haoutil.system.isPCBroswer()) {
-        // Cesium 1.61以后会默认关闭反走样，对于桌面端而言还是开启得好，
-        viewer.scene.postProcessStages.fxaa.enabled = true;
-
-        //鼠标滚轮放大的步长参数
-        viewer.scene.screenSpaceCameraController._zoomFactor = 2.0;
-
-        //IE浏览器优化
-        if (window.navigator.userAgent.toLowerCase().indexOf("msie") >= 0) {
-            viewer.targetFrameRate = 20;        //限制帧率
-            viewer.requestRenderMode = true;    //取消实时渲染
-        }
-
-    }
-    else {
-        //鼠标滚轮放大的步长参数
-        viewer.scene.screenSpaceCameraController._zoomFactor = 5.0;
-
-        //移动设备上禁掉以下几个选项，可以相对更加流畅 
-        viewer.requestRenderMode = true;    //取消实时渲染
-        viewer.scene.fog.enable = false;
-        viewer.scene.skyAtmosphere.show = false;
-        viewer.scene.globe.showGroundAtmosphere = false;
-    }
-
-    // 禁用默认的实体双击动作。
-    viewer.screenSpaceEventHandler.removeInputAction(Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK);
-    viewer.screenSpaceEventHandler.removeInputAction(Cesium.ScreenSpaceEventType.LEFT_CLICK);
-
-    //二三维切换不用动画
-    if (viewer.sceneModePicker)
-        viewer.sceneModePicker.viewModel.duration = 0.0;
-
-
-    $.ajax({
-        type: "get",
-        dataType: "json",
-        url: 'http://data.marsgis.cn/file/apidemo/qiye/point.json',
-        timeout: 0,
-        success: function (data) {
-            showPoint(data.Data)
+    //可在图层绑定右键菜单,对所有加到这个图层的矢量数据都生效
+    graphicLayer.bindContextMenu([
+        {
+            text: "删除对象",
+            iconCls: "fa fa-trash-o",
+            callback: function (e) {
+                let graphic = e.graphic;
+                if (graphic) {
+                    graphicLayer.removeGraphic(graphic);
+                }
+            },
         },
-        error: function (XMLHttpRequest, textStatus, errorThrown) {
-            haoutil.loading.hide();
-            haoutil.alert(configfile + "文件加载失败！");
-        }
-    });
+    ]);
 
+    //加一些演示数据
+    addGraphic_e01(graphicLayer)
+    addGraphic_e02(graphicLayer)
+    addGraphic_e03(graphicLayer)
+    addGraphic_e04(graphicLayer)
+    addGraphic_e05(graphicLayer)
+    addGraphic_e06(graphicLayer)
+    addGraphic_e07(graphicLayer)
+    addGraphic_e08(graphicLayer)
+    addGraphic_e09(graphicLayer)
+    addGraphic_e10(graphicLayer)
+    addGraphic_e11(graphicLayer)
+    addGraphic_e12(graphicLayer)
+    addGraphic_e13(graphicLayer)
+    addGraphic_e14(graphicLayer)
+    addGraphic_e15(graphicLayer)
 }
 
 
-function showPoint(arrdata) {
-    var dataSource = new Cesium.CustomDataSource();
-    viewer.dataSources.add(dataSource);
 
-    for (var item of arrdata) {
-        var position = Cesium.Cartesian3.fromDegrees(item.JD, item.WD, 2);
+function addGraphic_e01 (graphicLayer) {
+    let graphic = new mars3d.graphic.LabelEntity({
+        position: new mars3d.LatLngPoint(116.1, 31.0, 1000),
+        style: {
+            text: 'Mars3D三维地球平台软件',
+            font: 'normal small-caps normal 28px 楷体',
+            style: Cesium.LabelStyle.FILL_AND_OUTLINE,
+            fillColor: Cesium.Color.fromCssColorString('#003da6'),
+            outlineColor: Cesium.Color.fromCssColorString('#bfbfbf'),
+            outlineWidth: 2,
+            horizontalOrigin: Cesium.HorizontalOrigin.CENTER,
+            verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
+            disableDepthTestDistance: Number.POSITIVE_INFINITY //一直显示，不被地形等遮挡(会穿过地球被透视)
+        }
+    })
+    graphicLayer.addGraphic(graphic)
+}
 
-        //添加实体
-        var entity = dataSource.entities.add({
-            name: item.JC,
-            position: position,
-            point: {
-                //像素点
-                color: Cesium.Color.fromCssColorString("#3388ff"),
-                pixelSize: 10,
-                outlineColor: Cesium.Color.fromCssColorString("#ffffff"),
-                outlineWidth: 2,
-                scaleByDistance: new Cesium.NearFarScalar(1000, 1, 1000000, 0.1)
-            },
-            label: {
-                text: item.JC,
-                font: "16px 楷体",
-                style: Cesium.LabelStyle.FILL_AND_OUTLINE,
-                fillColor: Cesium.Color.AZURE,
-                outlineColor: Cesium.Color.BLACK,
-                outlineWidth: 2,
-                horizontalOrigin: Cesium.HorizontalOrigin.CENTER,
-                verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
-                pixelOffset: new Cesium.Cartesian2(0, -10), //偏移量
-                distanceDisplayCondition: new Cesium.DistanceDisplayCondition(
-                    0.0,
-                    200000
-                )
-            },
-            data: item,
-            popup: mars3d.util.getPopup("all", item, "企业点"),
-            click: function (entity) {
-                //单击回调
-                haoutil.msg("您单击了：" + entity.data.JC);
-            }
-        });
-    }
+function addGraphic_e02 (graphicLayer) {
+    let graphic = new mars3d.graphic.PointEntity({
+        position: [116.2, 31.0, 1000],
+        style: {
+            color: '#ff0000',
+            pixelSize: 10,
+            outlineColor: '#ffffff',
+            outlineWidth: 2
+        }
+    })
+    graphicLayer.addGraphic(graphic)
+}
 
+function addGraphic_e03 (graphicLayer) {
+    let graphic = new mars3d.graphic.BillboardEntity({
+        name: '贴地图标',
+        position: [116.3, 31.0, 1000],
+        style: {
+            image: 'http://mars3d.cn/example/img/marker/mark2.png',
+            scale: 1,
+            horizontalOrigin: Cesium.HorizontalOrigin.CENTER,
+            verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
+            clampToGround: true
+        }
+    })
+    graphicLayer.addGraphic(graphic)
+}
+
+function addGraphic_e04 (graphicLayer) {
+    let graphic = new mars3d.graphic.PlaneEntity({
+        position: new mars3d.LatLngPoint(116.4, 31.0, 1000),
+        style: {
+            plane: new Cesium.Plane(Cesium.Cartesian3.UNIT_Z, 0.0),
+            dimensions: new Cesium.Cartesian2(4000.0, 4000.0),
+            material: new Cesium.ImageMaterialProperty({
+                image: 'http://mars3d.cn/example/img/textures/movingRiver.png',
+                transparent: true
+            })
+        }
+    })
+    graphicLayer.addGraphic(graphic)
+}
+
+//
+function addGraphic_e05 (graphicLayer) {
+    let graphic = new mars3d.graphic.BoxEntity({
+        position: new mars3d.LatLngPoint(116.5, 31.0, 1000),
+        style: {
+            dimensions: new Cesium.Cartesian3(2000.0, 2000.0, 2000.0),
+            fill: true,
+            color: '#00ffff',
+            opacity: 0.9,
+            heading: 45,
+            roll: 45,
+            pitch: 0
+        }
+    })
+    graphicLayer.addGraphic(graphic)
+}
+
+function addGraphic_e06 (graphicLayer) {
+    let graphic = new mars3d.graphic.CircleEntity({
+        position: [116.1, 30.9, 1000],
+        style: {
+            radius: 1800.0,
+            color: '#00ff00',
+            opacity: 0.3,
+            outline: true,
+            outlineWidth: 3,
+            outlineColor: '#ffffff',
+            clampToGround: true
+        },
+        popup: '直接传参的popup'
+    })
+    graphicLayer.addGraphic(graphic)
+}
+
+function addGraphic_e07 (graphicLayer) {
+    let graphic = new mars3d.graphic.CylinderEntity({
+        position: [116.2, 30.9, 1000],
+        style: {
+            length: 3000.0,
+            topRadius: 0.0,
+            bottomRadius: 1300.0,
+            color: '#00FFFF',
+            opacity: 0.7
+        },
+        popup: '直接传参的popup'
+    })
+    graphicLayer.addGraphic(graphic)
+}
+
+//
+function addGraphic_e08 (graphicLayer) {
+    let graphic = new mars3d.graphic.EllipsoidEntity({
+        position: new mars3d.LatLngPoint(116.3, 30.9, 1000),
+        style: {
+            radii: new Cesium.Cartesian3(1500.0, 1500.0, 1500.0),
+            material: Cesium.Color.RED.withAlpha(0.5),
+            outline: true,
+            outlineColor: Cesium.Color.WHITE.withAlpha(0.3)
+        }
+    })
+    graphicLayer.addGraphic(graphic)
+}
+
+function addGraphic_e09 (graphicLayer) {
+    let graphic = new mars3d.graphic.ModelEntity({
+        name: '消防员',
+        position: [116.4, 30.9, 1000],
+        style: {
+            url: 'http://data.marsgis.cn/gltf/mars/firedrill/xiaofangyuan-run.gltf',
+            scale: 16,
+            minimumPixelSize: 100
+        }
+    })
+    graphicLayer.addGraphic(graphic)
+}
+
+function addGraphic_e10 (graphicLayer) {
+    let graphic = new mars3d.graphic.PolylineEntity({
+        positions: [
+            [116.5, 30.9, 1000],
+            [116.52, 30.91, 1000],
+            [116.53, 30.89, 1000]
+        ],
+        style: {
+            width: 5,
+            color: '#3388ff'
+        }
+    })
+    graphicLayer.addGraphic(graphic)
+    //graphic.addTo(graphicLayer)  //还可以另外一种写法,看个人习惯选用，内部都一样
+}
+
+function addGraphic_e11 (graphicLayer) {
+    let graphic = new mars3d.graphic.PolylineVolumeEntity({
+        positions: [
+            [116.1, 30.8, 1000],
+            [116.12, 30.81, 1000],
+            [116.13, 30.79, 1000]
+        ],
+        style: {
+            shape: 'pipeline',
+            radius: 80,
+            color: '#3388ff',
+            opacity: 0.9
+        }
+    })
+    graphicLayer.addGraphic(graphic)
+    //graphic.addTo(graphicLayer)  //还可以另外一种写法,看个人习惯选用，内部都一样
+}
+
+function addGraphic_e12 (graphicLayer) {
+    let graphic = new mars3d.graphic.CorridorEntity({
+        positions: [
+            [116.2, 30.8, 1000],
+            [116.22, 30.81, 1000],
+            [116.23, 30.79, 1000],
+            [116.247328, 30.806077, 610.41]
+        ],
+        style: {
+            width: 500,
+            color: '#3388ff'
+        }
+    })
+    graphicLayer.addGraphic(graphic)
+    //graphic.addTo(graphicLayer)  //还可以另外一种写法,看个人习惯选用，内部都一样
+}
+
+function addGraphic_e13 (graphicLayer) {
+    let graphic = new mars3d.graphic.WallEntity({
+        positions: [
+            [116.3, 30.8, 1000],
+            [116.31, 30.81, 1000],
+            [116.334639, 30.800735, 721.39],
+            [116.32, 30.79, 1000]
+        ],
+        style: {
+            closure: true,
+            diffHeight: 500,
+            //动画线材质
+            material: new mars3d.material.LineFlowMaterialProperty({
+                image: 'http://mars3d.cn/example/img/textures/fence.png',
+                color: Cesium.Color.fromCssColorString('#00ff00'),
+                speed: 10, //速度，建议取值范围1-100
+                axisY: true
+            })
+        }
+    })
+    graphicLayer.addGraphic(graphic)
+    //graphic.addTo(graphicLayer)  //还可以另外一种写法,看个人习惯选用，内部都一样
+}
+
+function addGraphic_e14 (graphicLayer) {
+    let graphic = new mars3d.graphic.RectangleEntity({
+        positions: [
+            [116.383144, 30.819978, 444.42],
+
+            [116.42216, 30.793431, 1048.07]
+        ],
+        style: {
+            color: '#3388ff',
+            opacity: 0.5,
+            outline: true,
+            outlineWidth: 3,
+            outlineColor: '#ffffff'
+        }
+    })
+    graphicLayer.addGraphic(graphic)
+    //graphic.addTo(graphicLayer)  //还可以另外一种写法,看个人习惯选用，内部都一样
+}
+
+function addGraphic_e15 (graphicLayer) {
+    let graphic = new mars3d.graphic.PolygonEntity({
+        positions: [
+            [116.510278, 30.834372, 567.29],
+            [116.530085, 30.809331, 448.31],
+            [116.507367, 30.788551, 98.21],
+            [116.472468, 30.823091, 677.39]
+        ],
+        style: {
+            material: new mars3d.material.WaterMaterialProperty({
+                //图片材质
+                normalMap: 'http://mars3d.cn/example/img/textures/waterNormals.jpg', // 水正常扰动的法线图
+                frequency: 8000.0, // 控制波数的数字。
+                animationSpeed: 0.02, // 控制水的动画速度的数字。
+                amplitude: 5.0, // 控制水波振幅的数字。
+                specularIntensity: 0.8, // 控制镜面反射强度的数字。
+                baseWaterColor: Cesium.Color.fromCssColorString('#006ab4'), // rgba颜色对象基础颜色的水。#00ffff,#00baff,#006ab4
+                blendColor: Cesium.Color.fromCssColorString('#006ab4') // 从水中混合到非水域时使用的rgba颜色对象。
+            })
+        }
+    })
+    graphicLayer.addGraphic(graphic)
+    //graphic.addTo(graphicLayer)  //还可以另外一种写法,看个人习惯选用，内部都一样
 }
